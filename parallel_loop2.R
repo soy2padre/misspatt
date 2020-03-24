bc105499284c61920c544ef5afe5b5d0db156e4e
Adam Davey <adavey@Adams-MacBook-Pro.local>
2020-03-24 13:26:28 -0400

    parallel_loop2.R changes error handling and object names

--- parallel_loop2.R
+++ parallel_loop2.R
@@ -8,17 +8,14 @@ if(!require('parallel')) install.packages('parallel', dependencies=TRUE)
 if(!require('foreach')) install.packages('foreach', dependencies=TRUE)
 ncores <- detectCores() - 1
 cl <- makeCluster(ncores)
-#clusterSetupRNG(cl, type='RNGstream', seed=c(304193, 937797, 840893, 650999, 679473, 35296))
 registerDoParallel(cl)
-#setwd('/Volumes/TUWORK/Documents/Missing_Data')
-#setwd('~/Missing_Data')
 source('simdata.R')
 source('mkmats.R')
 source('dopcamat.R')
 source('Kaiser_Jolliffe_Proflik.R')
 source('EKC.R')
 
-reps <- 1000
+reps <- 100
 c <- .4
 r <- .5
 
@@ -32,51 +29,37 @@ li <- length(nipc)
 lm <- length(pctmiss)
 totlen <- reps*ls*lf*li*lm
 
-# n <- c(100)
-# f <- c(3)
 pkgs <- c('paran', 'psych', 'MASS', 'parallel')
 
-#bigx <- matrix(nrow=0, ncol=11)
-#bigout <- matrix(nrow=0, ncol=11)
 myx <- matrix(nrow=0, ncol=11)
 
 rng <- RNGseq(totlen, 87658653)
 
 #mcoptions <- list(preschedule=FALSE, mc.set.seed=TRUE)
 
-myx <- foreach(pmiss=1:lm, .combine=rbind) %:%
+myx <- foreach(pm=1:lm, .combine=rbind) %:%
   foreach(n=1:ls, .combine=rbind) %:%
   foreach(f=1:lf, .combine=rbind) %:%
   foreach(ipc=1:li, .combine=rbind) %:%
-  foreach(iter=1:reps, .combine=rbind, .packages=pkgs, .inorder=FALSE, .errorhandling="pass") %dopar% {
-    k <- (pmiss - 1)*ls*lf*li*reps + 
+  foreach(iter=1:reps, .combine=rbind, .packages=pkgs, .inorder=FALSE, .errorhandling="remove") %dopar% {
+    k <- (pm - 1)*ls*lf*li*reps + 
       (n - 1)*lf*li*reps +
       (f - 1)*li*reps +
       (ipc - 1)*reps + iter
     rngtools::setRNG(rng[[k]])
-    # Generate Data
-    # mydata <- simdata(n=samp[n], f=nf[f], ipc=nipc[ipc], c=c, r=r, pmiss=pctmiss[pmiss])
-    # 
-    # # Generate Matrices
-    # mymats <- mkmats(mydata)
-    # 
-    # # Generate and Process Eigenvalues
-    # myout <- rbind(dopcamat(n=mymats$n, mat=mymats$cmat),
-    #                dopcamat(n=mymats$n, mat=mymats$rmat))
-    # cond <- rbind(cbind(iter, n, f=nf[f], ipc=nipc[ipc], pctmiss[pmiss], 'pearson'),
-    #               cbind(iter, n, f=nf[f], ipc=nipc[ipc], pctmiss[pmiss], 'tetrachoric'))
-    # colnames(cond)[5] <- 'pmiss'
-    # colnames(cond)[6] <- 'corrtype'
-    # #myout <- cbind(cond,myout)
-    # cbind(cond,myout)
-    # Handle Output
-    #bigout <- rbind(bigout, myout)
-    #rbind(bigout, myout)
-    cbind(iter, nipc[ipc], nf[f], samp[n], pctmiss[pmiss])
+    mydata <- simdata(n=samp[n], f=nf[f], ipc=nipc[ipc], c=c, r=r, pmiss=pctmiss[pm])
+    mymats <- mkmats(mydata)
+    myout <- rbind(dopcamat(n=mymats$n, mat=mymats$cmat),
+                   dopcamat(n=mymats$n, mat=mymats$rmat))
+    cond <- rbind(cbind(iter, n, f=nf[f], ipc=nipc[ipc], pctmiss[pm], 'pearson'),
+                  cbind(iter, n, f=nf[f], ipc=nipc[ipc], pctmiss[pm], 'tetrachoric'))
+    colnames(cond)[5] <- 'pmiss'
+    colnames(cond)[6] <- 'corrtype'
+    cbind(cond,myout)
+    #cbind(iter, nipc[ipc], nf[f], samp[n], pctmiss[pm])
   }
-#bigx <- rbind(bigx, myx)
 
-#save(myx, file='bigx4.Rdata')
+save(myx, file='bigx4.Rdata')
 stopImplicitCluster()
 stopCluster(cl)
 stop <- proc.time()
