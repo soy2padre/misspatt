82c1ce96ce487b7c07f9947ffdbd4fa0aeed56d5
Adam Davey <adavey@Adams-MacBook-Pro.local>
2020-04-26 15:30:42 -0400

    Converts output fields to numeric

--- Create_Summary_Table.R
+++ Create_Summary_Table.R
@@ -3,13 +3,14 @@ library(openxlsx)
 load('misspatt00.Rdata')
 colnames(misspatt)[2] <- 'n'
 
-myout <- misspatt %>%
+myout <- misspatt %>% 
+  mutate_at(.vars=c(1:5, 8:12), as.numeric)%>% 
   group_by(n, f, ipc, pmiss, corrtype, ptype) %>%
   summarize(nreps=n(),
-            pctparan = mean(f==nparan),
-            pctkaiser = mean(f==nkaiser),
-            pctjolliffe = mean(f==njolliffe),
-            pctproflik = mean(f==nproflik),
-            pctekc = mean(f==nekc))
+            pctparan = mean(abs(f-nparan)<=1),
+            pctkaiser = mean(abs(f-nkaiser)<=1),
+            pctjolliffe = mean(abs(f-njolliffe)<=1),
+            pctproflik = mean(abs(f-nproflik)<=1),
+            pctekc = mean(abs(f-nekc)<=1))
 
 write.xlsx(myout, file='summary_table.xlsx')
